8d1e0be960ed0d7afd53156bcb8dc6197f28a122
kiyooka sota <kiyosou0517@icloud.com>
2024-01-17 09:23:56 +0900

    add: 投稿一覧におけるページネーションとカルーセルの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all.includes(:user).order(created_at: :desc)
+    @posts = Post.all.includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   # GET /posts/1 or /posts/1.json
