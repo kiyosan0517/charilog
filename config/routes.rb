9e00525ec676b78f5194b6c920ff3d565ca7881f
kiyooka sota <kiyosou0517@icloud.com>
2024-01-24 18:01:08 +0900

    add: アイコン削除ボタンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[index new create show]
-  resource :profile, only: %i[edit update]
+  resource :profile, only: %i[edit update] do 
+    resources :attachments, controller: 'profile/attachments', only: %i[destroy]
+  end
   resources :posts do
     collection do
       post "upload_image"
