f17ff3bd361e9bc1ae3f2ba21925154aa381799b
kiyooka sota <kiyosou0517@icloud.com>
2024-02-04 22:25:11 +0900

    remove: 不要ファイルの削除・ログインページの一本化

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,7 +1,5 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
-
-  def new; end
+  skip_before_action :require_login, only: %i[create]
 
   def create
     @user = login(params[:email], params[:password])
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
 import ImagesController from "./images_controller"
 application.register("images", ImagesController)
