efa8b1b2ee6f5f9a81f753b57e844b6e50562ca3
kiyooka sota <kiyosou0517@icloud.com>
2024-03-04 19:15:05 +0900

    fix: 楽天APIが本番環境にて正常に動作しないため設定を調整

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -2,7 +2,7 @@ RakutenWebService.configure do |c|
   if Rails.env.development?
     c.application_id = ENV['RWS_APPLICATION_ID_DEV']
     c.affiliate_id = ENV['RWS_AFFILIATION_ID_DEV']
-  elsif Rails.env.production?
+  else
     c.application_id = ENV['RWS_APPLICATION_ID_PROD']
     c.affiliate_id = ENV['RWS_AFFILIATION_ID_PROD']
   end
