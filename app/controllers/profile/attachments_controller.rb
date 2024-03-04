dffdc740c6b47245436e6e8beefae70137e7b3d8
kiyooka sota <kiyosou0517@icloud.com>
2024-03-04 22:02:40 +0900

    fix: アイコン削除メッセージの追加・DB内にsample.pngがあれば再利用して、無ければ新たにsample.pngの画像データをアタッチする

--- app/controllers/profile/attachments_controller.rb
+++ app/controllers/profile/attachments_controller.rb
@@ -1,11 +1,16 @@
 class Profile::AttachmentsController < ApplicationController
   def destroy
-    avatar = ActiveStorage::Attachment.find(params[:id])
-    user = avatar.record
-    avatar.purge
+    user = User.find(params[:id])
+    user.avatar.purge
 
-    sample_avatar_path = Rails.root.join('app', 'assets', 'images', 'sample.png')
-    user.avatar.attach(io: File.open(sample_avatar_path), filename: 'sample.png')
+    sample_avatar = ActiveStorage::Blob.find_by(filename: 'sample.png')
+
+    if sample_avatar
+      user.avatar.attach(sample_avatar)
+    else
+      sample_avatar_path = Rails.root.join('app', 'assets', 'images', 'sample.png')
+      user.avatar.attach(io: File.open(sample_avatar_path), filename: 'sample.png')
+    end
 
     redirect_to edit_profile_path
   end
