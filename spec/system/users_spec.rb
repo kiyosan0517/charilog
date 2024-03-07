0be5bf4dec9ba2f386fc4e1dbab3d244acd27b4d
kiyooka sota <kiyosou0517@icloud.com>
2024-03-07 22:17:09 +0900

    fix: ログイン/ユーザー登録テストのエラーメッセージや画面遷移先などの情報を、実装内容に合わせる形で一部変更

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe 'Users', type: :system do
     context '入力値が正常' do
       it '新規登録が成功する' do
         visit new_user_path
-        fill_in 'ユーザーネーム', with: 'test'
+        fill_in 'ユーザー名', with: 'test'
         select 'PINARELLO', from: 'user[my_bike]'
         fill_in 'メールアドレス', with: 'test@example.com'
         fill_in 'パスワード', with: 'password'
         fill_in 'パスワード確認', with: 'password'
         click_button '登録する'
         expect(current_path).to eq(posts_path)
-        expect(page).to have_content('ログインしました')
+        expect(page).to have_content('ユーザー登録が完了しました')
       end
     end
     context 'ユーザーネーム未入力' do
@@ -26,48 +26,62 @@ RSpec.describe 'Users', type: :system do
         fill_in 'パスワード確認', with: 'password'
         click_button '登録する'
         expect(current_path).to eq(users_path)
-        expect(page).to have_content('登録に失敗しました')
-        expect(page).to have_content('ユーザーネームを入力してください')
+        expect(page).to have_content('ユーザー登録に失敗しました')
+        expect(page).to have_content('ユーザー名を入力してください')
       end
     end
     context 'メールアドレス未入力' do
       it '新規登録が失敗する' do
         visit new_user_path
-        fill_in 'ユーザーネーム', with: 'test'
+        fill_in 'ユーザー名', with: 'test'
         select 'PINARELLO', from: 'user[my_bike]'
         fill_in 'パスワード', with: 'password'
         fill_in 'パスワード確認', with: 'password'
         click_button '登録する'
         expect(current_path).to eq(users_path)
-        expect(page).to have_content('登録に失敗しました')
+        expect(page).to have_content('ユーザー登録に失敗しました')
         expect(page).to have_content('メールアドレスを入力してください')
       end
     end
     context 'メールアドレス重複' do
       it '新規登録が失敗する' do
         visit new_user_path
-        fill_in 'ユーザーネーム', with: 'test'
+        fill_in 'ユーザー名', with: 'test'
         select 'PINARELLO', from: 'user[my_bike]'
         fill_in 'メールアドレス', with: user.email
         fill_in 'パスワード', with: 'password'
         fill_in 'パスワード確認', with: 'password'
         click_button '登録する'
         expect(current_path).to eq(users_path)
-        expect(page).to have_content('登録に失敗しました')
+        expect(page).to have_content('ユーザー登録に失敗しました')
         expect(page).to have_content('メールアドレスはすでに存在します')
+      end
+    end
+    context 'パスワードとパスワード確認が不一致' do
+      it '新規登録が失敗する' do
+        visit new_user_path
+        fill_in 'ユーザー名', with: 'test'
+        select 'PINARELLO', from: 'user[my_bike]'
+        fill_in 'メールアドレス', with: 'test@example.com'
+        fill_in 'パスワード', with: 'password'
+        fill_in 'パスワード確認', with: 'testtest'
+        click_button '登録する'
+        expect(current_path).to eq(users_path)
+        expect(page).to have_content('ユーザー登録に失敗しました')
+        expect(page).to have_content('パスワード確認とパスワードの入力が一致しません')
       end
     end
     context 'パスワードが7字以下' do
       it '新規登録が失敗する' do
         visit new_user_path
-        fill_in 'ユーザーネーム', with: 'test'
+        fill_in 'ユーザー名', with: 'test'
         select 'PINARELLO', from: 'user[my_bike]'
         fill_in 'メールアドレス', with: 'test@example.com'
         fill_in 'パスワード', with: 'test'
         fill_in 'パスワード確認', with: 'test'
         click_button '登録する'
         expect(current_path).to eq(users_path)
-        expect(page).to have_content('登録に失敗しました')
+        expect(page).to have_content('ユーザー登録に失敗しました')
         expect(page).to have_content('パスワードは8文字以上で入力してください')
       end
     end
