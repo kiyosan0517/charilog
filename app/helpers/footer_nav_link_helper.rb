e2e1f64f0d6258526a6b8f99b5072601d52f4d72
kiyooka sota <kiyosou0517@icloud.com>
2024-03-13 15:15:28 +0900

    fix: ページネーション周辺の余白調整/配置を中央に変更・ナビゲーションフッターの記号を拡大

--- app/helpers/footer_nav_link_helper.rb
+++ app/helpers/footer_nav_link_helper.rb
@@ -3,7 +3,7 @@ module FooterNavLinkHelper
     content_tag(:div, class: 'col-2 p-0 text-center') do
       link_to path, style: 'text-decoration:none;' do
         content_tag(:div, class: 'text-white') do
-          content_tag(:div, class: 'h5 m-0') {
+          content_tag(:div, class: 'h4 m-0') {
             tag.ion_icon(name: icon_name)
           } +
           content_tag(:div, title, class: 'title font-mini-size')
