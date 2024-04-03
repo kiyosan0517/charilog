451ef5b7cd4524329631a24a1c452bf99e7a2bd7
kiyooka sota <kiyosou0517@icloud.com>
2024-04-03 13:19:50 +0900

    fix: 命名し忘れていたモジュール名を設定

--- app/controllers/concerns/set_tags.rb
+++ app/controllers/concerns/set_tags.rb
@@ -1,14 +1,16 @@
-def save_tags(sent_tags)
-  current_tags = @post.tags.pluck(:name) unless @post.tags.nil?
-  old_tags = current_tags - sent_tags
-  new_tags = sent_tags - current_tags
+module SetTags
+  def save_tags(sent_tags)
+    current_tags = @post.tags.pluck(:name) unless @post.tags.nil?
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
 
-  old_tags.each do |old|
-    @post.tags.delete Tag.find_by(name: old)
-  end
+    old_tags.each do |old|
+      @post.tags.delete Tag.find_by(name: old)
+    end
 
-  new_tags.each do |new|
-    new_tag = Tag.find_or_create_by(name: new)
-    @post.tags << new_tag
+    new_tags.each do |new|
+      new_tag = Tag.find_or_create_by(name: new)
+      @post.tags << new_tag
+    end
   end
 end
