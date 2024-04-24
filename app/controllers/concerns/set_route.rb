d83aba1ea6d4368bf96ad4e6b1f28d4a53d97757
kiyooka sota <kiyosou0517@icloud.com>
2024-04-24 19:05:55 +0900

    add: 移動ルート保存機能の経由地追加(カラム追加・コントローラー/ビュー編集)

--- app/controllers/concerns/set_route.rb
+++ app/controllers/concerns/set_route.rb
@@ -6,6 +6,10 @@ module SetRoute
         start_longitude: params[:post][:start_longitude],
         waypoint_latitude: params[:post][:waypoint_latitude],
         waypoint_longitude: params[:post][:waypoint_longitude],
+        waypoint1_latitude: params[:post][:waypoint1_latitude],
+        waypoint1_longitude: params[:post][:waypoint1_longitude],
+        waypoint2_latitude: params[:post][:waypoint2_latitude],
+        waypoint2_longitude: params[:post][:waypoint2_longitude],
         end_latitude: params[:post][:end_latitude],
         end_longitude: params[:post][:end_longitude],
         post_id: @post.id
